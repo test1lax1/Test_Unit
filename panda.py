fa1999e055a28841d38563cbf2a9fa39552cb25e
test1lax1 <test1lax1@gmail.com>
2020-09-15 14:40:46 +0530

    Add files via upload

--- panda.py
+++ panda.py
@@ -14,42 +14,42 @@ class SecondTest(unittest.TestCase):
     # Returns True or False.  
     def test(self):
         print("second test")
-        time.sleep(60)
+        time.sleep(10)
         self.assertTrue(True) 
 class ThirdTest(unittest.TestCase): 
   
     # Returns True or False.  
     def test(self):
         print("Third test")
-        time.sleep(60)
+        time.sleep(10)
         self.assertTrue(True) 
 class FourthTest(unittest.TestCase): 
   
     # Returns True or False.  
     def test(self):
         print("fourth test")
-        time.sleep(60)
+        time.sleep(10)
         self.assertTrue(True) 
 class FifthTest(unittest.TestCase): 
   
     # Returns True or False.  
     def test(self):
         print("fifth test")
-        time.sleep(60)
+        time.sleep(10)
         self.assertTrue(True) 
 class SixthTest(unittest.TestCase): 
   
     # Returns True or False.  
     def test(self):
         print("sixth test")
-        time.sleep(60)
+        time.sleep(10)
         self.assertTrue(True) 
 class SeventhTest(unittest.TestCase): 
   
     # Returns True or False.  
     def test(self):
         print("seventh test")
-        time.sleep(60)
+        time.sleep(10)
         self.assertTrue(True) 
   
 if __name__ == '__main__': 
